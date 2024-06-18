3dcfe7bfc417e5b9f6153d8ca9da540a9cf3da5e
Kim JaeHyung <you3192001@icloud.com>
2024-06-18 19:29:56 +0900

    Initial Commit

---
+++ SwiftUIArcodianTestWithTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIArcodianTestWithTCA
+//
+//  Created by Jae hyung Kim on 6/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIArcodianTestWithTCA/SwiftUIArcodianTestWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIArcodianTestWithTCAApp.swift
+//  SwiftUIArcodianTestWithTCA
+//
+//  Created by Jae hyung Kim on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIArcodianTestWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
