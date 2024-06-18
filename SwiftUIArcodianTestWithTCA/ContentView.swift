d5405ebd33abcabc3b615ee007f2dff3edb4cb60
Kim JaeHyung <you3192001@icloud.com>
2024-06-18 19:37:49 +0900

    Fetch: 자식 모델

--- SwiftUIArcodianTestWithTCA/ContentView.swift
+++ SwiftUIArcodianTestWithTCA/ContentView.swift
@@ -12,7 +12,21 @@ import SwiftUI
  각 섹션별 유동적 데이터
  아코디온 뷰를 구성해 보겠습니다.
  */
+// 사용할 모델을 정의해 보겠습니다.
+// 부모와 자식 모델을 정의해 볼께요
 
+struct AppType {
+    let id = UUID()
+    let name: String
+    let currentNum = Int.random(in: 0...100)
+    var items: [AppChildType]?
+}
+
+struct AppChildType {
+    let id = UUID()
+    let name: String
+    let intro: String
+}
 
 struct ContentView: View {
     var body: some View {
